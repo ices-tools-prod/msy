391507f3eed9b6f36b55ebc8afb021988aaca833
Andy Campbell <andrew.campbell@marine.ie>
2020-06-05 23:46:59 +0100

    Utility function

--- R/utilities.R
+++ R/utilities.R
@@ -16,3 +16,28 @@ loader <- function(p)
  utils::flush.console()
  if (floor(p) == 1) cat("\n")
 }
+
+fGetValsScan <- function(Nums,RPs){
+  
+  #Nums - character vector with values or the name of refererence points to be substituted
+  #RPs - named vector of reference points 
+
+  if (length(Nums)==1){
+    if (is.na(Nums)){
+      return(NA)
+    }
+  }
+
+  #extract the numeric ones
+  ret <- as.numeric(Nums[!is.na(suppressWarnings(as.numeric(Nums)))])
+  
+  #now the specified reference points
+  if (sum(is.na(suppressWarnings(as.numeric(Nums))))>0) {
+    ret <- c(ret,as.numeric(RPs[Nums[is.na(suppressWarnings(as.numeric(Nums)))]]))
+  }
+  
+  #add a warning if there are unmatched RPs
+  
+  sort(ret)
+  
+}
